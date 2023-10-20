d064a6c6309eeb36f465f45470a078a12d45ec19
alumno <alumno@Instituto.local>
2023-10-20 09:33:38 -0300

    Version 1.1
    Falta el 3° ejercicio

--- script.js
+++ script.js
@@ -5,9 +5,41 @@
 <= (Mayor igual)
 => (Menor igual)
 != (Distinto)
-
-
-
-
-
+&& = y
 */
+
+//Resoluciones 20/10/2023
+
+//1
+function Calc_Num_Mayor ()
+{
+    let num1 = 5 
+    let num2 = 6
+    
+    if (num1 > num2)
+    {
+        alert (num1 + ' es mayor que ' + num2) 
+    } else {
+        alert (num2 + ' es mayor que ' + num1)
+    }
+}
+
+
+//2
+
+function PositivoyNegativo(num1){
+    if (num1<0) { ('El numero es Negaivo')
+} else {
+    ('El numero es Positivo')
+}
+}
+
+function Num0 (num3) {
+    if (num=0) {
+        ('El numero es 0')
+    }
+}
+
+//3
+
+function 
